0b8a49d47599bdda84a869c8614a4fb45f230b71
Derek Werneck <85125331+Z3rm4k@users.noreply.github.com>
2023-09-25 23:15:41 -0300

    Adição da questão doze, feita no vscode
    Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58

---
+++ Questao12.py
@@ -0,0 +1,5 @@
+# Pergunta a altura da pessoa em metros e faz o calculo
+altura = float(input("Digite a altura da pessoa em metros: "))
+peso = (72.7 * altura) - 58
+# Mostra o peso ideal com base na altura
+print(f"O peso ideal para uma pessoa com altura {altura} metros é de {peso:.3} quilogramas.")
