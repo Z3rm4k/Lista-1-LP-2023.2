a8d5353f3d8c998c45634b435b584fc994849b20
Derek Werneck <85125331+Z3rm4k@users.noreply.github.com>
2023-09-25 23:32:25 -0300

    Adição da questão quatorze, feita no vscode
    João Papo-de-Pescador, homem de bem, comprou um microcomputador para controlar o rendimento diário de seu trabalho. Toda vez que ele traz um peso de peixes maior que o estabelecido pelo regulamento de pesca do estado de São Paulo (50 quilos) deve pagar uma multa de R$ 4,00 por quilo excedente. João precisa que você faça um programa que leia a variável peso (peso de peixes) e calcule o excesso. Gravar na variável excesso a quantidade de quilos além do limite e na variável multa o valor da multa que João deverá pagar. Imprima os dados do programa com as mensagens adequadas.

---
+++ Questao14.py
@@ -0,0 +1,17 @@
+# Pede o peso dos peixes ao usuário
+peixes = float(input("Digite o peso de peixes em quilogramas: "))
+
+limite = 50.0
+if peixes > limite:
+    excesso = peixes - limite
+    multa = excesso * 4.0
+else:
+    excesso = 0
+    multa = 0
+
+# Printa o resultado
+if excesso > 0:
+    print(f"Peso excedente de {excesso:.3f} quilogramas.")
+    print(f"Valor da multa a pagar: R$ {multa:.1f}")
+else:
+    print("Peso dentro do limite permitido em São Paulo. Sem valor a pagar.")
